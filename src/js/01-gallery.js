879c160299be9e0f3dcfdd26e457ab104d561f43
angelo0730 <137179844+angelo0730@users.noreply.github.com>
2023-12-11 23:33:14 +0800

    update task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+// Described in documentation;
+
+import SimpleLightbox from "simplelightbox";
+// Additional styles import
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const createGallery = (element) => {
+    return element.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    })
+    .join("");
+};
+
+const photosList = createGallery(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", photosList);
+
+
+// ----Lightbox and Delay-----
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionsDelay: 250,
+});
+
+lightbox.on("show.simplelightbox")
